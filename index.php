a6495f3014a9aa25ae7a548a965e9273e4161d98
Elisa <elisabozon@eduroam-107086.grenet.fr>
2017-10-20 17:12:12 +0200

    add index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <html>
 <head>
 	<title>bozon digital</title>
+
+	<script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="./node_modules/popper.js/dist/umd/popper.min.js"></script>
+	<script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- <script type="text/javascript" src="./node_modules/bootstrap/js/dist/modal.js"></script>
+	<script type="text/javascript" src="./node_modules/flexslider/jquery.flexslider-min.js"></script> -->
+	<script type="text/javascript" src="./lib/script.js"></script>
+
+	<!-- <link rel="stylesheet" type="text/css" href="./node_modules/flexslider/flexslider.css"> -->
+	<link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="./lib/style.css">
 </head>
 <body>
 
